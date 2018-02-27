8e2c3c3ff7602acaf8a9988c2adafec2140bec7e
thomasvl <thomasvl@google.com>
2018-02-27 10:08:16 -0500

    Travis improvements.
    - Move install into a stand alone script to expand it.
      - Support installing the latest bazel release.
    - Move the script step into a stand along script to expand it.
      - Support doing a build or running buildifier.
    - Update the config to:
      - Use the latest bazel release and head for tests (won't have
        to update the config with each bazel release).
      - Add a buildifer test to ensure the files are good.
    
    PiperOrigin-RevId: 187171515

---
+++ .travis_build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Copyright 2018 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+# -------------------------------------------------------------------------------------------------
+# Asked to do a bazel build.
+if [[ -n "${BAZEL:-}" ]]; then
+  # - Turn off our test sharding to not swamp the machine
+  # - Crank down the progress messages to not flood the travis log, but still
+  #   provide some output so there is an indicator things aren't hung.
+  # - "--test_output=errors" causes failures to report more completely since
+  #   just getting the log file info isn't that useful on CI.
+  set -x
+  if [[ -n "${BUILD_TARGET:-}" ]]; then
+    bazel \
+      --bazelrc=/dev/null \
+      build \
+      --show_progress_rate_limit=30.0 \
+      "${BUILD_TARGET}"
+  fi
+  bazel \
+    --bazelrc=/dev/null \
+    test \
+    --show_progress_rate_limit=30.0 \
+    --define bazel_rules_apple.apple_shell_test.enable_sharding=0 \
+    --test_output=errors \
+    "${TARGET}"
+  set +x
+fi
+
+# -------------------------------------------------------------------------------------------------
+# Asked to do a buildifier run.
+if [[ -n "${BUILDIFER:-}" ]]; then
+  # bazelbuild/buildtools/issues/220 - diff doesn't include the file that needs updating
+  # bazelbuild/buildtools/issues/221 - the exist status is always zero.
+  if [[ -n "$(find . -name BUILD -print | xargs buildifier -v -d)" ]]; then
+    echo "ERROR: BUILD file formatting issue(s)"
+    find . -name BUILD -print -exec buildifier -v -d {} \;
+    exit 1
+  fi
+fi
