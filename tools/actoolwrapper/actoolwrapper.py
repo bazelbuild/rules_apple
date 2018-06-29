a6928bc30ca93bace634864483b815c6317f3cb3
dabelknap <dabelknap@google.com>
2018-06-29 19:01:40 -0400

    Consolidate "ibtool" and "actool" into a single wrapper. The existing "ibtoolwrapper" and "actoolwrapper" can still be called directly, but they redirect to the new wrapper.
    RELNOTES: None
    PiperOrigin-RevId: 202722151

--- tools/actoolwrapper/actoolwrapper.py
+++ tools/actoolwrapper/actoolwrapper.py
@@ -25,100 +25,14 @@ and you must have Xcode installed.
   <args>: Additional arguments to pass to "actool".
 """
 
-import os
-import re
 import sys
-from build_bazel_rules_apple.tools.wrapper_common import execute
+from build_bazel_rules_apple.tools.xctoolrunner import xctoolrunner
 
 
-def _output_filtering(raw_stdout):
-  """Filter the stdout messages from "actool".
-
-  Args:
-    raw_stdout: This is the unmodified stdout captured from "xcrun actool".
-
-  Returns:
-    The filtered output string.
-  """
-  section_header = re.compile("^/\\* ([^ ]*) \\*/$")
-
-  excluded_sections = ["com.apple.actool.compilation-results"]
-
-  spurious_patterns = map(re.compile, [
-      r"\[\]\[ipad\]\[76x76\]\[\]\[\]\[1x\]\[\]\[\]: notice: \(null\)",
-      r"\[\]\[ipad\]\[76x76\]\[\]\[\]\[1x\]\[\]\[\]: notice: 76x76@1x app icons"
-      " only apply to iPad apps targeting releases of iOS prior to 10.0.",
-  ])
-
-  def is_spurious_message(line):
-    for pattern in spurious_patterns:
-      match = pattern.search(line)
-      if match is not None:
-        return True
-    return False
-
-  output = []
-  current_section = None
-  data_in_section = False
-
-  for line in raw_stdout.splitlines():
-    header_match = section_header.search(line)
-
-    if header_match:
-      data_in_section = False
-      current_section = header_match.group(1)
-      continue
-
-    if current_section and current_section not in excluded_sections:
-      if is_spurious_message(line):
-        continue
-
-      if not data_in_section:
-        data_in_section = True
-        output.append("/* %s */\n" % current_section)
-
-      output.append(line + "\n")
-
-  return "".join(output)
-
-
-def _main(outdir, toolargs):
-  """Assemble the call to "xcrun actool"."""
-
-  xcrunargs = ["xcrun",
-               "actool",
-               "--errors",
-               "--warnings",
-               "--notices",
-               "--compress-pngs",
-               "--output-format",
-               "human-readable-text",
-               "--compile",
-               outdir]
-
-  xcrunargs += toolargs
-
-  # If we are running into problems figuring out "actool" issues, there are a
-  # couple of environment variables that may help. Both of the following must be
-  # set to work.
-  #   IBToolDebugLogFile=<OUTPUT FILE PATH>
-  #   IBToolDebugLogLevel=4
-  # You may also see if
-  #   IBToolNeverDeque=1
-  # helps.
-  # Yes, IBTOOL appears to be correct here due to "actool" and "ibtool" being
-  # based on the same codebase.
-  execute.execute_and_filter_output(xcrunargs,
-                                    filtering=_output_filtering,
-                                    trim_paths=True)
-
-
-def validate_args(args):
-  if len(args) < 2:
-    sys.stderr.write("ERROR: Output directory path required.")
-    sys.exit(1)
+def _main():
+  # TODO(dabelknap) Update the Bazel rules to call xctoolrunner directly.
+  xctoolrunner.main(["actool", "--compile"] + sys.argv[1:])
 
 
 if __name__ == "__main__":
-  validate_args(sys.argv)
-  _main(sys.argv[1], sys.argv[2:])
+  _main()
