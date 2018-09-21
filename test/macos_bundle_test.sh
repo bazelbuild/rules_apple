2f7c2c91dbb5624ba3e5577443b5445c7e76f38a
A Googler <no-reply@google.com>
2018-09-21 18:10:40 -0400

    macos_bundle_test: Fix `warning: ignoring -fapple-kext which is valid for C++ and Objective-C++ only`
    PiperOrigin-RevId: 214051326

--- test/macos_bundle_test.sh
+++ test/macos_bundle_test.sh
@@ -485,16 +485,19 @@ EOF
 function test_kernel_extension() {
   mkdir -p kext
 
-  cat > kext/kext-builder_info.c <<EOF
+  cat > kext/kext-builder_info.cc <<EOF
 #include <mach/mach_types.h>
 
+extern "C" {
 extern kern_return_t _start(kmod_info_t *ki, void *data);
 extern kern_return_t _stop(kmod_info_t *ki, void *data);
 
-__attribute__((visibility("default"))) KMOD_EXPLICIT_DECL(com.google.kext_builder, "1.0.0d1", _start, _stop)
-__private_extern__ kmod_start_func_t *_realmain = 0;
+__attribute__((visibility("default")))
+KMOD_EXPLICIT_DECL(com.google.kext_builder, "1.0.0d1", _start,
+                   _stop) __private_extern__ kmod_start_func_t *_realmain = 0;
 __private_extern__ kmod_stop_func_t *_antimain = 0;
-__private_extern__ int _kext_apple_cc = __APPLE_CC__ ;
+__private_extern__ int _kext_apple_cc = __APPLE_CC__;
+}
 EOF
 
   cat > kext/KextBuilder.h <<EOF
@@ -575,7 +578,7 @@ cc_library(
     srcs = [
         "KextBuilder.cc",
         "KextBuilder.h",
-        "kext-builder_info.c",
+        "kext-builder_info.cc",
     ],
     copts = [
         "-mkernel",
