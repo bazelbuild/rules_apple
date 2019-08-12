6e2df773267d78fa6057e67c2f8b238123977671
Gabriel Giosia <giosia@google.com>
2019-08-12 10:58:46 -0700

    Move apple_bundle_version tests to Skylark where possible.
    Two issues are making it impossible to migrate all tests:
     - The `embed_label` flag is not in the configuration and can't be modified in a transition
     - Some of the failures we test for happen when the action is run(the tool throws errors) and the Skylark testing framework currently doesn't have a way to test failures outside of the Analysis stage.
    
    RELNOTES: None
    PiperOrigin-RevId: 262960532

--- test/apple_bundle_version_test.sh
+++ test/apple_bundle_version_test.sh
@@ -63,39 +63,6 @@ function tear_down() {
   rm -rf pkg
 }
 
-# Tests that manual version numbers work correctly.
-function test_manual_version_numbers() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_version = "1.2.3",
-    short_version_string = "1.2",
-)
-EOF
-
-  do_build ios //pkg:saved_version || \
-      fail "Should build"
-  assert_contains "\"build_version\": \"1.2.3\"" test-bin/pkg/saved_version.txt
-  assert_contains "\"short_version_string\": \"1.2\"" \
-      test-bin/pkg/saved_version.txt
-}
-
-# Tests that short_version_string defaults to the same value as build_version
-# if not specified.
-function test_short_version_string_defaults_to_build_version() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_version = "1.2.3",
-)
-EOF
-
-  do_build ios //pkg:saved_version || fail "Should build"
-  assert_contains "\"build_version\": \"1.2.3\"" test-bin/pkg/saved_version.txt
-  assert_contains "\"short_version_string\": \"1.2.3\"" \
-      test-bin/pkg/saved_version.txt
-}
-
 # Test that the build label passed via --embed_label can be parsed out
 # correctly.
 function test_build_label_substitution() {
@@ -116,31 +83,6 @@ EOF
       fail "Should build"
   assert_contains "\"build_version\": \"1.2.3\"" test-bin/pkg/saved_version.txt
   assert_contains "\"short_version_string\": \"1.2\"" \
-      test-bin/pkg/saved_version.txt
-}
-
-# Test that the fallback_build_label is used when --embed_label is not
-# passed on the build.
-function test_build_label_substitution_from_fallback_label() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_label_pattern = "MyApp_{version}_RC0*{candidate}",
-    build_version = "{version}.{candidate}",
-    fallback_build_label = "MyApp_99.99_RC99",
-    short_version_string = "{version}",
-    capture_groups = {
-        "version": "\\d+\.\\d+",
-        "candidate": "\\d+",
-    },
-)
-EOF
-
-  do_build ios //pkg:saved_version || \
-      fail "Should build"
-  assert_contains "\"build_version\": \"99.99.99\"" \
-      test-bin/pkg/saved_version.txt
-  assert_contains "\"short_version_string\": \"99.99\"" \
       test-bin/pkg/saved_version.txt
 }
 
@@ -168,101 +110,10 @@ EOF
       test-bin/pkg/saved_version.txt
 }
 
-# Tests that short_version_string defaults to the same value as build_version
-# if not specified.
-function test_short_version_string_defaults_to_build_version_with_label_substitution() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_label_pattern = "MyApp_{version}_RC0*{candidate}",
-    build_version = "{version}.{candidate}",
-    capture_groups = {
-        "version": "\\d+\.\\d+",
-        "candidate": "\\d+",
-    },
-)
-EOF
-
-  do_build ios //pkg:saved_version --embed_label=MyApp_1.2_RC03 || \
-      fail "Should build"
-  assert_contains "\"build_version\": \"1.2.3\"" test-bin/pkg/saved_version.txt
-  assert_contains "\"short_version_string\": \"1.2.3\"" \
-      test-bin/pkg/saved_version.txt
-}
-
-# Tests that a build_label_pattern that contains placeholders not found in
-# capture_groups fails.
-function test_pattern_referencing_missing_capture_groups_fails() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_label_pattern = "MyApp_{version}_RC00",
-    build_version = "{version}.{candidate}",
-    capture_groups = {
-        "version": "\\d+",
-    },
-)
-EOF
-
-  ! do_build ios //pkg:saved_version --embed_label=MyApp_1.2_RC03 || \
-      fail "Should fail"
-  expect_log "Some groups were not defined in capture_groups: \[candidate\]"
-}
-
-# Tests that the build fails if build_label_pattern is provided but
-# capture_groups is not.
-function test_build_label_pattern_requires_capture_groups() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_label_pattern = "MyApp_{version}_RC0*{candidate}",
-    build_version = "{version}.{candidate}",
-)
-EOF
-
-  ! do_build ios //pkg:saved_version --embed_label=MyApp_1.2_RC03 || \
-      fail "Should fail"
-  expect_log "If either build_label_pattern or capture_groups is provided, then both must be provided"
-}
-
-# Tests that the build fails if capture_groups is provided but
-# build_label_pattern is not.
-function test_capture_groups_requires_build_label_pattern() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_version = "{version}.{candidate}",
-    capture_groups = {
-        "foo": "bar",
-    },
-)
-EOF
-
-  ! do_build ios //pkg:saved_version --embed_label=MyApp_1.2_RC03 || \
-      fail "Should fail"
-  expect_log "If either build_label_pattern or capture_groups is provided, then both must be provided"
-}
-
-# Tests that the build fails if fallback_build_label is provided but
-# build_label_pattern is not.
-function test_fallback_build_label_requires_build_label_pattern() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_version = "1.2",
-    fallback_build_label = "MyApp_1.2_RC03"
-)
-EOF
-
-  ! do_build ios //pkg:saved_version || \
-      fail "Should fail"
-  expect_log "If fallback_build_label is provided, then build_label_pattern and capture_groups must be provided."
-}
-
 
 # Test that the build fails if the build label does not match the regular
 # expression that is built after substituting the regex groups for the
-# placeholders.
+# placeholders. This fails during tool execution.
 function test_build_label_that_does_not_match_regex_fails() {
   cat >> pkg/BUILD <<EOF
 apple_bundle_version(
@@ -282,48 +133,5 @@ EOF
   expect_log "The build label (\"MyApp_1.2_RC03\") did not match the pattern"
 }
 
-# Test that substitution does not occur if there is a build label pattern but
-# --embed_label is not specified on the command line. (This supports local
-# builds).
-function test_no_substitution_if_build_label_not_present() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_label_pattern = "MyApp_{version}_RC0*{candidate}",
-    build_version = "{version}.{candidate}",
-    short_version_string = "{version}",
-    capture_groups = {
-        "version": "\\d+\.\\d+",
-        "candidate": "\\d+",
-    },
-)
-EOF
-
-  do_build ios //pkg:saved_version || fail "Should build"
-  assert_contains "{}" test-bin/pkg/saved_version.txt
-}
-
-# Test that the presence of a build label pattern does not short circuit the
-# use of a literal version string, even if no --embed_label argument is
-# provided.
-function test_build_label_pattern_does_not_short_circuit_literal_version() {
-  cat >> pkg/BUILD <<EOF
-apple_bundle_version(
-    name = "bundle_version",
-    build_label_pattern = "MyApp_{version}_RC0*{candidate}",
-    build_version = "1.2.3",
-    short_version_string = "1.2",
-    capture_groups = {
-        "version": "\\d+\.\\d+",
-        "candidate": "\\d+",
-    },
-)
-EOF
-
-  do_build ios //pkg:saved_version || fail "Should build"
-  assert_contains "\"build_version\": \"1.2.3\"" test-bin/pkg/saved_version.txt
-  assert_contains "\"short_version_string\": \"1.2\"" \
-      test-bin/pkg/saved_version.txt
-}
 
 run_suite "apple_bundle_version tests"
