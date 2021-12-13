38783ce1d3a963dc8136e6b1caa5fe2ae921c18d
Kayla McArthur <klmcarthur@google.com>
2021-12-13 11:38:01 -0800

    Generate DER when signing with entitlements to support pre macOS 12.0 watchOS builds.
    PiperOrigin-RevId: 416094144
    (cherry picked from commit ca3fa899688837654f7d9c7f0e03bf91feb82fda)

--- tools/codesigningtool/codesigningtool.py
+++ tools/codesigningtool/codesigningtool.py
@@ -62,6 +62,7 @@ def _invoke_codesign(codesign_path, identity, entitlements, force_signing,
   cmd = [codesign_path, "-v", "--sign", identity]
   if entitlements:
     cmd.extend([
+        "--generate-entitlement-der",
         "--entitlements",
         entitlements,
     ])
