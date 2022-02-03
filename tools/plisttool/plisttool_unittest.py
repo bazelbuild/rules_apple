bazelbuild/rules_apple

commit e61b0d48e4a1ee271871178eb1994f236715d744

Add entitlements matching validation with profile
This change adds support for `wifi-info`, `nfc`, and `devicecheck`
entitlements validations.

Additionally it refactors `aps-environment` validation to an generic
entitlement validation against provisioning profile values. Supporting
single value entitlements, as well as list based entitlements.

Based on PRs #849 #1229 #1231 #1233 by keith

PiperOrigin-RevId: 398807387

tools/plisttool/plisttool.py
@@ -277,14 +277,31 @@ ENTITLEMENTS_HAS_GROUP_PROFILE_DOES_NOT = (
     'support use of this key.'
 )
 
-ENTITLEMENTS_APS_ENVIRONMENT_MISSING = (
-    'Target "%s" uses entitlements with the aps-environment key, but the '
-    'profile does not have this key'
+ENTITLEMENTS_MISSING = (
+    'Target "%s" uses entitlements with the '
+    '"%s" key, but the profile does not have this key'
 )
 
-ENTITLEMENTS_APS_ENVIRONMENT_MISMATCH = (
-    'In target "%s"; the entitlements "aps-environment" ("%s") did not '
-    'match the value in the provisioning profile ("%s").'
+ENTITLEMENTS_VALUE_MISMATCH = (
+    'In target "%s"; the entitlement value for "%s" ("%s") '
+    'did not match the value in the provisioning profile ("%s").'
+)
+
+ENTITLEMENTS_VALUE_NOT_IN_LIST = (
+    'In target "%s"; the entitlement value for "%s" ("%s") '
+    'is not in the provisioning profiles potential values ("%s").'
+)
+
+_ENTITLEMENTS_TO_VALIDATE_WITH_PROFILE = (
+    'aps-environment',
+    'com.apple.developer.networking.wifi-info',
+    'com.apple.developer.passkit.pass-presentation-suppression',
+    'com.apple.developer.payment-pass-provisioning',
+    'com.apple.developer.siri',
+    'com.apple.developer.usernotifications.time-sensitive',
+    # Keys which have a list of potential values in the profile, but only one
+    # can be defined in the entitlements file, and must be part of that list.
+    'com.apple.developer.devicecheck.appattest-environment',
 )
 
 ENTITLEMENTS_BETA_REPORTS_ACTIVE_MISMATCH = (
@@ -1195,15 +1212,11 @@ class EntitlementsTask(PlistToolTask):
             ENTITLEMENTS_APP_ID_PROFILE_MISMATCH % (
               self.target, src_app_id, profile_app_id))
 
-    aps_environment = entitlements.get('aps-environment')
-    if aps_environment and profile_entitlements:
-      profile_aps_environment = profile_entitlements.get('aps-environment')
-      if not profile_aps_environment:
-        self._report(ENTITLEMENTS_APS_ENVIRONMENT_MISSING % self.target)
-      elif aps_environment != profile_aps_environment:
-        self._report(
-            ENTITLEMENTS_APS_ENVIRONMENT_MISMATCH %
-            (self.target, aps_environment, profile_aps_environment))
+    for entitlement in _ENTITLEMENTS_TO_VALIDATE_WITH_PROFILE:
+      self._check_entitlement_matches_profile_value(
+          entitlement=entitlement,
+          entitlements=entitlements,
+          profile_entitlements=profile_entitlements)
 
     # If beta-reports-active is in either the profile or the entitlements file
     # it must be in both or the upload will get rejected by Apple
@@ -1236,6 +1249,48 @@ class EntitlementsTask(PlistToolTask):
         'com.apple.developer.associated-domains', self.target,
         supports_wildcards=True,
         allow_wildcards_in_entitlements=True)
+
+    # com.apple.developer.nfc.readersession.formats
+    self._check_entitlements_array(
+        entitlements,
+        profile_entitlements,
+        'com.apple.developer.nfc.readersession.formats',
+        self.target)
+
+  def _check_entitlement_matches_profile_value(
+      self,
+      entitlement,
+      entitlements,
+      profile_entitlements):
+    """Checks if an entitlement value matches against profile entitlement.
+
+    If provisioning profile entitlement is defined as a list, this will
+    check if entitlement is part of that list.
+
+    Args:
+      entitlement: Entitlement key identifier.
+      entitlements: Entitlements dictionary.
+      profile_entitlements: Provisioning Profile entitlements dictionary.
+    """
+    entitlements_value = entitlements.get(entitlement)
+    if entitlements_value is None:
+      return
+
+    profile_value = (profile_entitlements or {}).get(entitlement)
+    if profile_value is None:
+      # provisioning profile does not have entitlement.
+      self._report(ENTITLEMENTS_MISSING % (self.target, entitlement))
+    elif (isinstance(profile_value, list)
+          and entitlements_value not in profile_value):
+      # provisioning profile does not have entitlement in list.
+      self._report(
+          ENTITLEMENTS_VALUE_NOT_IN_LIST % (
+              self.target, entitlement, entitlements_value, profile_value))
+    elif entitlements_value != profile_value:
+      # provisioning profile entitlement does not match value.
+      self._report(
+          ENTITLEMENTS_VALUE_MISMATCH % (
+              self.target, entitlement, entitlements_value, profile_value))
 
   def _does_id_match(self,
                      id,

tools/plisttool/plisttool_unittest.py
@@ -1718,6 +1718,28 @@ class PlistToolTest(unittest.TestCase):
           },
       })
 
+  def test_nfc_matching(self):
+    # This is really looking for the lack of an error being raised.
+    plist1 = {
+        'com.apple.developer.nfc.readersession.formats': ['NDEF'],
+    }
+    self._assert_plisttool_result(
+        {
+            'plists': [plist1],
+            'entitlements_options': {
+                'bundle_id': 'my.bundle.id',
+                'profile_metadata_file': {
+                    'Entitlements': {
+                        'com.apple.developer.nfc.readersession.formats': [
+                            'NDEF',
+                            'TAG',
+                        ],
+                    },
+                    'Version': 1,
+                },
+            },
+        }, plist1)
+
   def test_entitlements_aps_environment_matches(self):
     plist = {'aps-environment': 'production'}
     self._assert_plisttool_result({
@@ -1736,7 +1758,8 @@ class PlistToolTest(unittest.TestCase):
     with self.assertRaisesRegex(
         plisttool.PlistToolError,
         re.escape(
-            plisttool.ENTITLEMENTS_APS_ENVIRONMENT_MISSING % _testing_target)):
+            plisttool.ENTITLEMENTS_MISSING % (
+                _testing_target, 'aps-environment'))):
       plist = {'aps-environment': 'production'}
       self._assert_plisttool_result({
           'plists': [plist],
@@ -1753,8 +1776,8 @@ class PlistToolTest(unittest.TestCase):
   def test_entitlements_aps_environment_mismatch(self):
     with self.assertRaisesRegex(
         plisttool.PlistToolError,
-        re.escape(plisttool.ENTITLEMENTS_APS_ENVIRONMENT_MISMATCH % (
-            _testing_target, 'production', 'development'))):
+        re.escape(plisttool.ENTITLEMENTS_VALUE_MISMATCH % (
+            _testing_target, 'aps-environment', 'production', 'development'))):
       plist = {'aps-environment': 'production'}
       self._assert_plisttool_result({
           'plists': [plist],
@@ -1811,6 +1834,59 @@ class PlistToolTest(unittest.TestCase):
           'plists': [plist],
           'entitlements_options': {
               'profile_metadata_file': {
+                  'Version': 1,
+              },
+          },
+      }, plist)
+
+  def test_entitlements_missing_wifi_info_active(self):
+    plist = {}
+    self._assert_plisttool_result({
+        'plists': [plist],
+        'entitlements_options': {
+            'profile_metadata_file': {
+                'Entitlements': {
+                    'com.apple.developer.networking.wifi-info': True,
+                },
+                'Version': 1,
+            },
+        },
+    }, plist)
+
+  def test_entitlements_wifi_info_active_mismatch(self):
+    with self.assertRaisesRegex(
+        plisttool.PlistToolError,
+        re.escape(plisttool.ENTITLEMENTS_VALUE_MISMATCH % (
+            _testing_target, 'com.apple.developer.networking.wifi-info',
+            'False', 'True'))):
+      plist = {'com.apple.developer.networking.wifi-info': False}
+      self._assert_plisttool_result({
+          'plists': [plist],
+          'entitlements_options': {
+              'profile_metadata_file': {
+                  'Entitlements': {
+                      'com.apple.developer.networking.wifi-info': True,
+                  },
+                  'Version': 1,
+              },
+          },
+      }, plist)
+
+  def test_entitlements_profile_missing_wifi_info_active(self):
+    with self.assertRaisesRegex(
+        plisttool.PlistToolError,
+        re.escape(
+            plisttool.ENTITLEMENTS_MISSING %
+            (_testing_target, 'com.apple.developer.networking.wifi-info'))):
+      plist = {'com.apple.developer.networking.wifi-info': True}
+      self._assert_plisttool_result({
+          'plists': [plist],
+          'entitlements_options': {
+              'profile_metadata_file': {
+                  'Entitlements': {
+                      'application-identifier': 'QWERTY.*',
+                      # No wifi-info
+                  },
                   'Version': 1,
               },
           },

commit 5587358e12f861d21359b7de887d04f3a774d402

Fix entitlement validation with appattest
https://github.com/bazelbuild/rules_apple/commit/e61b0d48e4a1ee271871178eb1994f236715d744 introduced a regression where
the value of this entitlement was in the potential list, so it fell
through to the next conditional which validated they were equal, which
will never be the case. This also adds a test for this successful case.

Based on PR #1308

PiperOrigin-RevId: 426183779

tools/plisttool/plisttool.py
@@ -299,8 +299,8 @@ _ENTITLEMENTS_TO_VALIDATE_WITH_PROFILE = (
     'com.apple.developer.payment-pass-provisioning',
     'com.apple.developer.siri',
     'com.apple.developer.usernotifications.time-sensitive',
-    # Keys which have a list of potential values in the profile, but only one
-    # can be defined in the entitlements file, and must be part of that list.
+    # Keys which have a list of potential values in the profile, but only one in
+    # the entitlements that must be in the profile's list of values
     'com.apple.developer.devicecheck.appattest-environment',
 )
 
@@ -534,7 +534,7 @@ def _load_json(string_or_file):
 
 
 class PlistToolError(ValueError):
-# pylint: disable=g-bad-exception-name
+  # pylint: disable=g-bad-exception-name
   """Raised for all errors.
 
   Custom ValueError used to allow catching (and logging) just the plisttool
@@ -1299,17 +1299,28 @@ class EntitlementsTask(PlistToolTask):
     if profile_value is None:
       # provisioning profile does not have entitlement.
       self._report(ENTITLEMENTS_MISSING % (self.target, entitlement))
-    elif (isinstance(profile_value, list)
-          and entitlements_value not in profile_value):
-      # provisioning profile does not have entitlement in list.
-      self._report(
-          ENTITLEMENTS_VALUE_NOT_IN_LIST % (
-              self.target, entitlement, entitlements_value, profile_value))
-    elif entitlements_value != profile_value:
-      # provisioning profile entitlement does not match value.
-      self._report(
-          ENTITLEMENTS_VALUE_MISMATCH % (
-              self.target, entitlement, entitlements_value, profile_value))
+
+    elif isinstance(profile_value, list):
+      if (isinstance(entitlements_value, str) and
+          entitlements_value not in profile_value):
+        # provisioning profile does not have entitlement in list.
+        self._report(
+            ENTITLEMENTS_VALUE_NOT_IN_LIST %
+            (self.target, entitlement, entitlements_value, profile_value))
+
+      if isinstance(entitlements_value, list):
+        self._check_entitlements_array(
+            entitlements,
+            profile_entitlements,
+            entitlement,
+            self.target)
+
+    elif isinstance(profile_value, (str, bool)):
+      if entitlements_value != profile_value:
+        # provisioning profile entitlement does not match value.
+        self._report(
+            ENTITLEMENTS_VALUE_MISMATCH % (
+                self.target, entitlement, entitlements_value, profile_value))
 
   def _does_id_match(self,
                      entitlement_id,

tools/plisttool/plisttool_unittest.py
@@ -1779,6 +1779,45 @@ class PlistToolTest(unittest.TestCase):
           },
       }, plist)
 
+  def test_attest_valid(self):
+    plist = {
+        'com.apple.developer.devicecheck.appattest-environment': 'development'}
+    self._assert_plisttool_result(
+        {
+            'plists': [plist],
+            'entitlements_options': {
+                'profile_metadata_file': {
+                    'Entitlements': {
+                        'com.apple.developer.devicecheck.appattest-environment':
+                            ['development', 'production'],
+                    },
+                    'Version': 1,
+                },
+            },
+        }, plist)
+
+  def test_attest_mismatch(self):
+    with self.assertRaisesRegex(
+        plisttool.PlistToolError,
+        re.escape(plisttool.ENTITLEMENTS_VALUE_NOT_IN_LIST %
+                  (_testing_target,
+                   'com.apple.developer.devicecheck.appattest-environment',
+                   'foo', ['development']))):
+      plist = {'com.apple.developer.devicecheck.appattest-environment': 'foo'}
+      self._assert_plisttool_result(
+          {
+              'plists': [plist],
+              'entitlements_options': {
+                  'profile_metadata_file': {
+                      'Entitlements': {
+                          'com.apple.developer.devicecheck.appattest-environment':
+                              ['development'],
+                      },
+                      'Version': 1,
+                  },
+              },
+          }, plist)
+
   def test_entitlements_missing_beta_reports_active(self):
     plist = {}
     self._assert_plisttool_result({