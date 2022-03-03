fff993cbf7652fc681c0ab9547f294c2e0ad5c71
Billy Irwin <birwin93@gmail.com>
2022-03-03 12:32:16 -0700

    Support skipping tests with --test_filter (#1371)
    Co-authored-by: Billy Irwin <billy.irwin@robinhood.com>

--- test/ios_test_runner_unit_test.sh
+++ test/ios_test_runner_unit_test.sh
@@ -458,6 +458,48 @@ function test_ios_unit_test_with_host_with_filter() {
   expect_log "Executed 1 test, with 0 failures"
 }
 
+function test_ios_unit_test_with_host_and_skip_filter() {
+  create_sim_runners
+  create_test_host_app
+  create_ios_unit_tests
+  do_ios_test --test_filter=-PassingUnitTest/testPass //ios:PassingWithHost || fail "should pass"
+
+  expect_not_log "Test Case '-\[PassingUnitTest testPass\]' passed"
+  expect_log "Test Case '-\[PassingUnitTest testPass2\]' passed"
+  expect_log "Test Case '-\[PassingUnitTest testPass3\]' passed"
+  expect_log "Test Suite 'PassingUnitTest' passed"
+  expect_log "Test Suite 'PassingWithHost.xctest' passed"
+  expect_log "Executed 3 tests, with 0 failures"
+}
+
+function test_ios_unit_test_with_host_and_multi_skip_filter() {
+  create_sim_runners
+  create_test_host_app
+  create_ios_unit_tests
+  do_ios_test --test_filter=-PassingUnitTest/testPass,-PassingUnitTest/testPass2 //ios:PassingWithHost || fail "should pass"
+
+  expect_not_log "Test Case '-\[PassingUnitTest testPass\]' passed"
+  expect_not_log "Test Case '-\[PassingUnitTest testPass2\]' passed"
+  expect_log "Test Case '-\[PassingUnitTest testPass3\]' passed"
+  expect_log "Test Suite 'PassingUnitTest' passed"
+  expect_log "Test Suite 'PassingWithHost.xctest' passed"
+  expect_log "Executed 2 tests, with 0 failures"
+}
+
+function test_ios_unit_test_with_host_and_skip_and_only_filters() {
+  create_sim_runners
+  create_test_host_app
+  create_ios_unit_tests
+  do_ios_test --test_filter=PassingUnitTest,-PassingUnitTest/testPass2 //ios:PassingWithHost || fail "should pass"
+
+  expect_log "Test Case '-\[PassingUnitTest testPass\]' passed"
+  expect_not_log "Test Case '-\[PassingUnitTest testPass2\]' passed"
+  expect_log "Test Case '-\[PassingUnitTest testPass3\]' passed"
+  expect_log "Test Suite 'PassingUnitTest' passed"
+  expect_log "Test Suite 'PassingWithHost.xctest' passed"
+  expect_log "Executed 3 tests, with 0 failures"
+}
+
 function test_ios_unit_test_with_env() {
   create_sim_runners
   create_ios_unit_envtest ENV_KEY1 ENV_VALUE2
