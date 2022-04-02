a93ad60b28877b0cfa16abafc6bdd3fce0e66c76
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2022-04-01 20:56:25 -0700

    Allow for JSON lcov coverage exports via a flag (#1390)
    Allows JSON coverage results to be created in
    `$TEST_UNDECLARED_OUTPUTS_DIR` 
    when passing `--test_env=COVERAGE_PRODUCE_JSON=TRUE`

--- apple/testing/default_runner/ios_test_runner.template.sh
+++ apple/testing/default_runner/ios_test_runner.template.sh
@@ -129,7 +129,7 @@ fi
 
 # Use the TESTBRIDGE_TEST_ONLY environment variable set by Bazel's --test_filter
 # flag to set tests_to_run value in ios_test_runner's launch_options.
-# Any test prefixed with '-' will be passed to "skip_tests". Otherwise the tests 
+# Any test prefixed with '-' will be passed to "skip_tests". Otherwise the tests
 # is passed to "tests_to_run"
 if [[ -n "$TESTBRIDGE_TEST_ONLY" ]]; then
   if [[ -n "${LAUNCH_OPTIONS_JSON_STR}" ]]; then
@@ -228,7 +228,6 @@ readonly profdata="$TMP_DIR/coverage.profdata"
 xcrun llvm-profdata merge "$profraw" --output "$profdata"
 
 lcov_args=(
-  -format lcov
   -instr-profile "$profdata"
   -ignore-filename-regex='.*external/.+'
   -path-equivalence="$ROOT",.
@@ -254,6 +253,7 @@ readonly error_file="$TMP_DIR/llvm-cov-error.txt"
 llvm_cov_status=0
 xcrun llvm-cov \
   export \
+  -format lcov \
   "${lcov_args[@]}" \
   @"$COVERAGE_MANIFEST" \
   > "$COVERAGE_OUTPUT_FILE" \
@@ -266,4 +266,21 @@ if [[ -s "$error_file" || "$llvm_cov_status" -ne 0 ]]; then
   echo "error: while exporting coverage report" >&2
   cat "$error_file" >&2
   exit 1
+fi
+
+if [[ -n "${COVERAGE_PRODUCE_JSON:-}" ]]; then
+  llvm_cov_json_export_status=0
+  xcrun llvm-cov \
+    export \
+    -format text \
+    "${lcov_args[@]}" \
+    @"$COVERAGE_MANIFEST" \
+    > "$TEST_UNDECLARED_OUTPUTS_DIR/coverage.json"
+    2> "$error_file" \
+    || llvm_cov_json_export_status=$?
+  if [[ -s "$error_file" || "$llvm_cov_json_export_status" -ne 0 ]]; then
+    echo "error: while exporting json coverage report" >&2
+    cat "$error_file" >&2
+    exit 1
+  fi
 fi
