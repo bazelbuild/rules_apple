5a373002ec843ed36b6253412add879b524d19e8
Mauricio Garcia <mauriciogs@google.com>
2022-04-20 14:51:59 -0700

    Add tests to verify builds for XCFrameworks with static libraries.
    This change adds additional tests to `apple_static_xcframework` rule,\
    to verify static libraries builds for simulator/device using Mach-O load\
    commands.
    
    Currently Mach-O files include either `LC_BUILD_VERSION` or\
    `LC_VERSION_MIN_IPHONEOS` depending on the target environment and target\
    minimum iOS version.
    
    PiperOrigin-RevId: 443200094
    (cherry picked from commit 702370f4faf6623be42a73f6fd1e96d00746713e)

--- test/starlark_tests/verifier_scripts/archive_contents_test.sh
+++ test/starlark_tests/verifier_scripts/archive_contents_test.sh
@@ -198,10 +198,20 @@ if [[ -n "${BINARY_TEST_FILE-}" ]]; then
   fi
 
   if [[ -n "${MACHO_LOAD_COMMANDS_CONTAIN-}" || -n "${MACHO_LOAD_COMMANDS_NOT_CONTAIN-}" ]]; then
-    # Remove the leftmost white space from otool output to make string matching
-    # of symbols possible, avoiding the accidental elimination of white space
-    # from paths and identifiers.
-    IFS=$'\n' actual_symbols=($(otool -l "$path" | awk '{$1=$1}1'))
+    # The `otool` commands below remove the leftmost white space from the
+    # output to make string matching of symbols possible, avoiding the
+    # accidental elimination of white space from paths and identifiers.
+    IFS=$'\n'
+    if [[ -n "${BINARY_TEST_ARCHITECTURE-}" ]]; then
+      arch=$(eval echo "$BINARY_TEST_ARCHITECTURE")
+      if [[ ! -n $arch ]]; then
+        fail "No architecture specified for binary file at \"$path\""
+      else
+        actual_symbols=($(otool -arch "$arch" -l "$path" | awk '{$1=$1}1'))
+      fi
+    else
+      actual_symbols=($(otool -l "$path" | awk '{$1=$1}1'))
+    fi
     if [[ -n "${MACHO_LOAD_COMMANDS_CONTAIN-}" ]]; then
       for test_symbol in "${MACHO_LOAD_COMMANDS_CONTAIN[@]}"
       do
