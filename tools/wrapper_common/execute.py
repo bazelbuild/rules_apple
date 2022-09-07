f2df18242b54f5d1d7e81b2054caaf606a1606a1
Dave MacLachlan <dmaclach@google.com>
2022-09-07 13:03:05 -0700

    Fix up actool compiler to error on missing children warnings and bad json.
    Previously actool just spit out a warning about missing children and bad json, but we have found many bugs that have been ignored because of them just being a warning.
    
    PiperOrigin-RevId: 472793623

--- tools/wrapper_common/execute.py
+++ tools/wrapper_common/execute.py
@@ -18,7 +18,6 @@ import re
 import subprocess
 import sys
 
-
 _DEFAULT_TIMEOUT = 900
 
 
@@ -36,10 +35,10 @@ def execute_and_filter_output(cmd_args,
     cmd_args: A list of strings beginning with the command to execute followed
       by its arguments.
     filtering: Optionally specify a filter for stdout/stderr. It must be
-        callable and have the following signature:  myFilter(tool_exit_status,
-          stdout_string, stderr_string) -> (stdout_string, stderr_string)  The
-          filter can then use the tool's exit status to process the output as
-          they wish, returning what ever should be used.
+      callable and have the following signature:  myFilter(tool_exit_status,
+      stdout_string, stderr_string) -> (tool_exit_status, stdout_string,
+      stderr_string) The filter can then use the tool's exit status to process
+      the output as they wish, returning what ever should be used.
     trim_paths: Optionally specify whether or not to trim the current working
       directory from any paths in the output. Based on output after filtering,
       if a filter has been specified.
@@ -91,7 +90,7 @@ def execute_and_filter_output(cmd_args,
   if (stdout or stderr) and filtering:
     if not callable(filtering):
       raise TypeError("'filtering' must be callable.")
-    stdout, stderr = filtering(cmd_result, stdout, stderr)
+    cmd_result, stdout, stderr = filtering(cmd_result, stdout, stderr)
 
   if trim_paths:
     stdout = _trim_paths(stdout)
@@ -130,7 +129,7 @@ def _trim_paths(stdout):
   def replace_path(m):
     path = m.group(0)
     # Some paths present in stdout may contain symlinks, which must be resolved
-    # before we can reliably compare to CWD.
+    # before we can reliably compare to current_working_dir.
     fullpath = os.path.realpath(path)
     # realpath will remove terminating "/" so we must add it back.
     if path.endswith("/") and not fullpath.endswith("/"):
