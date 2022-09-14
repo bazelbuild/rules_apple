8d807c37a23b887b4cfe4d68fbbd318c141278ea
Nicholas Levin <nglevin@google.com>
2022-09-14 10:35:27 -0700

    New examples for the watchos_single_target_application rule, based off the existing watchos_application and watchos_extension examples to better demonstrate migration for Bazel clients.
    PiperOrigin-RevId: 474331020
    (cherry picked from commit c16772e4857d4b44ae7f0db9a648a0b879e66818)

--- examples/watchos/HelloWorld/WatchSharedSources/InterfaceController.m
+++ examples/watchos/HelloWorld/WatchSharedSources/InterfaceController.m
@@ -1,4 +1,4 @@
-// Copyright 2017 The Bazel Authors. All rights reserved.
+// Copyright 2022 The Bazel Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 #import <WatchConnectivity/WatchConnectivity.h>
 
-#import "examples/watchos/HelloWorld/WatchSources/InterfaceController.h"
+#import "examples/watchos/HelloWorld/WatchSharedSources/InterfaceController.h"
 
 @interface InterfaceController () <WCSessionDelegate>
 @end
