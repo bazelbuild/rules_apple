67a75e2119d7d0eee8335a882ab5c530c44af1cd
Patrick Balestra <me@patrickbalestra.com>
2023-02-11 13:58:15 +0100

    Debug CI

---
+++ .bazelci/xcode_debug.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+xcrun simctl list
+
+# xcrun simctl create "Apple Watch Series 7" "com.apple.CoreSimulator.SimDeviceType.Apple-Watch-Series-7-45mm" "watchOS9.1"
+
+xcrun simctl list devices available
