c16f74b4556ae0634f656aa38d8d82e50aac2544
Matt Robinson <mattrob@hey.com>
2023-03-17 09:21:11 -0600

    Alphabetize better

--- apple/testing/default_runner/ios_xctestrun_runner.template.sh
+++ apple/testing/default_runner/ios_xctestrun_runner.template.sh
@@ -274,11 +274,11 @@ if [[ "$should_use_xcodebuild" == true ]]; then
   readonly xctestrun_file="$test_tmp_dir/tests.xctestrun"
   /usr/bin/sed \
     -e "s@BAZEL_INSERT_LIBRARIES@$xctestrun_libraries@g" \
-    -e "s@BAZEL_TEST_PRODUCT_MODULE_NAME@$test_bundle_name@g" \
     -e "s@BAZEL_TEST_BUNDLE_PATH@__TESTROOT__/$test_bundle_name.xctest@g" \
     -e "s@BAZEL_TEST_ENVIRONMENT@$xctestrun_env@g" \
     -e "s@BAZEL_TEST_HOST_BASED@$xctestrun_test_host_based@g" \
     -e "s@BAZEL_TEST_HOST_PATH@$xctestrun_test_host_path@g" \
+    -e "s@BAZEL_TEST_PRODUCT_MODULE_NAME@$test_bundle_name@g" \
     -e "s@BAZEL_IS_XCTRUNNER_HOSTED_BUNDLE@$xcrun_is_xctrunner_hosted_bundle@g" \
     -e "s@BAZEL_IS_UI_TEST_BUNDLE@$xcrun_is_ui_test_bundle@g" \
     -e "s@BAZEL_TARGET_APP_PATH@$xcrun_target_app_path@g" \
