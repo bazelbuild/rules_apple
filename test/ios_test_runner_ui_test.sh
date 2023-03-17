ed1a1c53a1825bed467831089423e3d330e3fe41
Matt Robinson <mattrob@hey.com>
2023-03-17 08:14:25 -0600

    Remove test/ios_test_runner_ui_test.sh change

--- test/ios_test_runner_ui_test.sh
+++ test/ios_test_runner_ui_test.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Integration tests for iOS xctestrun runner.
+# Integration tests for iOS test runner.
 
 function set_up() {
   mkdir -p ios
