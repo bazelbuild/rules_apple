9e26f98b1467a97e835f842096fd2cf05e118a2c
Patrick Balestra <me@patrickbalestra.com>
2023-04-02 18:53:25 +0200

    Create sim?

--- .bazelci/xcode_debug.sh
+++ .bazelci/xcode_debug.sh
@@ -2,7 +2,5 @@
 
 xcrun simctl list
 
-# xcrun simctl create "Apple Watch Series 7" "com.apple.CoreSimulator.SimDeviceType.Apple-Watch-Series-7-45mm" "watchOS9.1"
-
 xcrun simctl list devices available
 ls /Applications/
