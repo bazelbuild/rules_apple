413b858d7ba041d57efdc3a2b30b8cc0a538b543
Dave MacLachlan <dmaclach@google.com>
2023-07-17 06:05:38 -0700

    Internal change
    PiperOrigin-RevId: 548665877
    (cherry picked from commit e3101f7b12542b840bf7a551e3309cc4c5227859)

--- apple/internal/templates/macos.template.sh
+++ apple/internal/templates/macos.template.sh
@@ -39,6 +39,4 @@ readonly BUNDLE_INFO_PLIST="${APP_DIR}/Contents/Info.plist"
 readonly BUNDLE_EXECUTABLE=$(/usr/libexec/PlistBuddy -c "Print :CFBundleExecutable" "${BUNDLE_INFO_PLIST}")
 
 # Launch the app binary
-# Do *not* use `exec` here becaues we need the clean up done by the trap to run after the 
-# executable has been run.
-"${APP_DIR}/Contents/MacOS/${BUNDLE_EXECUTABLE}" "$@"
+exec "${APP_DIR}/Contents/MacOS/${BUNDLE_EXECUTABLE}" "$@"
