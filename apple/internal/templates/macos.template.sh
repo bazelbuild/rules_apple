15bc9f8271af022d553a2a5658c1ba10f4dee318
Dave MacLachlan <dmaclach@google.com>
2023-07-27 10:40:38 -0700

    No public description
    PiperOrigin-RevId: 551575860
    (cherry picked from commit 13a93d72f056f613a9a9aa5a50f953fd58516092)

--- apple/internal/templates/macos.template.sh
+++ apple/internal/templates/macos.template.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eux
+set -eu
 
 if [[ "$(uname)" != Darwin ]]; then
   echo "Cannot run macOS targets on a non-mac machine."
@@ -24,7 +24,6 @@ fi
 if [ -d '%app_path%' ]; then
   # App bundles are directories with the .app extension
   readonly APP_DIR="%app_path%"
-  readonly DSYM_PATH="%app_path%.dSYM"
 else
   readonly TEMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/bazel_temp.XXXXXX")
   trap 'rm -rf "${TEMP_DIR}"' EXIT
@@ -32,29 +31,17 @@ else
   # Unpack the archive
   unzip -qq '%app_path%' -d "${TEMP_DIR}"
   readonly APP_DIR="${TEMP_DIR}/%app_name%.app"
-
-  # Copy the dsym if there is one
-  readonly DSYM_NAME='%app_name%.app.dSYM'
-  readonly DSYM_DIR="$(dirname %app_path%)"
-  readonly DSYM_PATH="${DSYM_DIR}/${DSYM_NAME}"
-  if [ -d "${DSYM_PATH}" ]; then
-    pushd "${DSYM_DIR}"
-    # pax does a much faster copy than cp and is more dependable than cp -c
-    # https://github.com/nico/hack/blob/main/notes/copydir.md
-    pax -rwl "${DSYM_NAME}" "${TEMP_DIR}"
-    popd
-  fi
 fi
 
-if [ -d "${DSYM_PATH}" ]; then
+for dsym in $(find -L "$(dirname %app_path%)" -type d -name "*.dSYM"); do
   # This adds the symbols to the CoreSymbolication so they can be easily found
   # without having spotlight index them.
   # Tagged with "bazel" for easy deletions of all bazel symbols should we want
   # to add a cleanup mechanism.
   # Not cleaning up as part of the trap because the symbols may be accessed
   # after the trap has run in the case of a crash.
-  symbolscache add --tag Bazel "${APP_DIR}.dSYM/Contents/Resources/DWARF/"*
- fi
+  symbolscache add --tag Bazel "${dsym}/Contents/Resources/DWARF/"*
+done
 
 # Get the bundle executable name of the app. Read this from the plist in case it
 # differs from the app name.
