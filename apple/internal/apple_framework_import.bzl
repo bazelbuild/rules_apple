bazelbuild/rules_apple

commit 5926b990842514cea21cb27f2ca70b70179bb4e8

Add support for swiftinterface files from (xc)frameworks file processing (#2043)
Originally we chose not to take this upstream commit because we want to
maintain support for pre-compiling your own internal frameworks without
library evolution. Due to future commits on the upstream branch that we
do want this commit takes swiftinterface only support while still
maintaining swiftmodule support

(cherry picked from commit 6c85e96dda26495f72ad77c041c750b53400314f)

Co-authored-by: Mauricio Garcia <mauriciogs@google.com>

apple/internal/apple_framework_import.bzl
@@ -327,7 +327,9 @@ def _apple_static_framework_import_impl(ctx):
     additional_cc_infos = []
     additional_objc_providers = []
     additional_objc_provider_fields = {}
-    if framework_imports_by_category.swift_module_imports or has_swift:
+    if framework_imports_by_category.swift_interface_imports or \
+       framework_imports_by_category.swift_module_imports or \
+       has_swift:
         toolchain = ctx.attr._toolchain[SwiftToolchainInfo]
         providers.append(SwiftUsageInfo())
 

apple/internal/apple_xcframework_import.bzl
@@ -140,6 +140,8 @@ def _get_xcframework_library(
             headers: List of File referencing XCFramework library header files. This can be either
                 a single tree artifact or a list of regular artifacts.
             clang_module_map: File referencing the XCFramework library Clang modulemap file.
+            swift_module_interface: File referencing the XCFramework library Swift module interface
+                file (`.swiftinterface`).
     """
     xcframework_library = None
     if not parse_xcframework_info_plist:
@@ -200,6 +202,13 @@ def _get_xcframework_library_from_paths(*, target_triplet, xcframework):
            f.basename.startswith(target_triplet.architecture)
     ]
 
+    swift_module_interfaces = [
+        f
+        for f in files_by_category.swift_interface_imports
+        if _matches_library(f) and
+           f.basename.startswith(target_triplet.architecture)
+    ]
+
     framework_files = filter_by_library_identifier(xcframework.files)
 
     includes = []
@@ -218,6 +227,7 @@ def _get_xcframework_library_from_paths(*, target_triplet, xcframework):
         includes = includes,
         clang_module_map = module_maps[0] if module_maps else None,
         swiftmodule = swiftmodules,
+        swift_module_interface = swift_module_interfaces[0] if swift_module_interfaces else None,
     )
 
 def _get_xcframework_library_with_xcframework_processor(
@@ -337,6 +347,7 @@ def _get_xcframework_library_with_xcframework_processor(
         includes = includes,
         clang_module_map = module_map_file,
         swiftmodule = [],
+        swift_module_interface = None,
         framework_files = [],
     )
 
@@ -520,7 +531,9 @@ def _apple_static_xcframework_import_impl(ctx):
 
     additional_cc_infos = []
     additional_objc_providers = []
-    if xcframework.files_by_category.swift_module_imports or has_swift:
+    if xcframework.files_by_category.swift_interface_imports or \
+       xcframework.files_by_category.swift_module_imports or \
+       has_swift:
         swift_toolchain = ctx.attr._toolchain[SwiftToolchainInfo]
         providers.append(SwiftUsageInfo())
 

apple/internal/framework_import_support.bzl
@@ -145,6 +145,7 @@ def _classify_file_imports(config_vars, import_files):
             - header_imports: Objective-C(++) header imports.
             - module_map_imports: Clang modulemap imports.
             - swift_module_imports: Swift module imports.
+            - swift_interface_imports: Swift module interface imports.
             - bundling_imports: Unclassified imports.
     """
     bundling_imports = []
@@ -152,6 +153,7 @@ def _classify_file_imports(config_vars, import_files):
     header_imports = []
     module_map_imports = []
     swift_module_imports = []
+    swift_interface_imports = []
     for file in import_files:
         # Extension matching
         file_extension = file.extension
@@ -175,15 +177,22 @@ def _classify_file_imports(config_vars, import_files):
                 header_imports.append(file)
             module_map_imports.append(file)
             continue
-        if file_extension in ["swiftmodule", "swiftinterface"]:
+        if file_extension == "swiftmodule":
             # Add Swift's module files to header_imports so
             # that they are correctly included in the build
             # by Bazel but they aren't processed in any way
             header_imports.append(file)
             swift_module_imports.append(file)
             continue
+        if file_extension == "swiftinterface":
+            # Add Swift's interface files to header_imports so
+            # that they are correctly included in the build
+            # by Bazel but they aren't processed in any way
+            header_imports.append(file)
+            swift_interface_imports.append(file)
+            continue
         if file_extension in ["swiftdoc", "swiftsourceinfo"]:
-            # Ignore swiftdoc files, they don't matter in the build, only for IDEs
+            # Ignore swiftdoc files, they don't matter in the build, only for IDEs.
             continue
         if file_extension == "a":
             binary_imports.append(file)
@@ -202,6 +211,7 @@ def _classify_file_imports(config_vars, import_files):
         binary_imports = binary_imports,
         header_imports = header_imports,
         module_map_imports = module_map_imports,
+        swift_interface_imports = swift_interface_imports,
         swift_module_imports = swift_module_imports,
         bundling_imports = bundling_imports,
     )
@@ -220,6 +230,7 @@ def _classify_framework_imports(config_vars, framework_imports):
             - header_imports: Apple framework header imports.
             - module_map_imports: Apple framework modulemap imports.
             - swift_module_imports: Apple framework swiftmodule imports.
+            - swift_interface_imports: Apple framework Swift module interface imports.
     """
     framework_imports_by_category = _classify_file_imports(config_vars, framework_imports)
 
@@ -250,6 +261,7 @@ def _classify_framework_imports(config_vars, framework_imports):
         bundling_imports = bundling_imports,
         header_imports = framework_imports_by_category.header_imports,
         module_map_imports = framework_imports_by_category.module_map_imports,
+        swift_interface_imports = framework_imports_by_category.swift_interface_imports,
         swift_module_imports = framework_imports_by_category.swift_module_imports,
     )
 

test/testdata/fmwk/generate_framework.bzl
@@ -98,14 +98,12 @@ def _generate_import_framework_impl(ctx):
             ),
         )
 
-        # Mock swiftmodule file. Imported `.swiftmodule` files are not supported due to Swift
-        # toolchain compatibility and Swift's ABI stability through `.swiftinterface` files.
-        # However, Apple framework import rules use Swift interface files to flag an imported
-        # framework contains Swift, and thus propagate Swift toolchain specific flags up the
-        # build graph.
+        # Apple framework and XCFrameworks import rules use Swift interface files to flag an
+        # imported framework contains Swift, and thus propagate Swift toolchain specific flags up
+        # the build graph.
         if include_module_interface_files:
-            module_interface = actions.declare_file(architectures[0] + ".swiftmodule")
-            actions.write(output = module_interface, content = "I'm a mock .swiftmodule file")
+            module_interface = actions.declare_file(architectures[0] + ".swiftinterface")
+            actions.write(output = module_interface, content = "I'm a mock .swiftinterface file")
             module_interfaces.append(module_interface)
 
     # Create framework bundle
@@ -163,7 +161,7 @@ Info.plist file to test resource propagation.
         ),
         "swift_library": attr.label(
             allow_files = True,
-            doc = "Label for a Swift library target to source archive and swiftmodule files from.",
+            doc = "Label for a Swift library target to source archive and module files from.",
             providers = [SwiftInfo],
         ),
         "libtype": attr.string(

commit e1180326fb5a1399b28752c0503ad8791dfe41d6

Correctly add `.swiftinterface` files to imported dynamic xcframework `public_hdrs`
5926b990842514cea21cb27f2ca70b70179bb4e8 broke importing xcframeworks that have swiftinterface files when using `parse_xcframework_info_plist`. We didn’t notice it before because the tests were using an invalid build setting name.

This change prevents the `.swiftinterface` files from breaking in `xcframework_processor_tool.py`, while still including them in the `CcInfo`.

I mimicked where were setting `swiftmodule_imports` before (in particular I didn’t set it for `xcframework_static_import`). It seems we might be over setting it in some places (or under-setting it there).

Signed-off-by: Brentley Jones <github@brentleyjones.com>

apple/internal/apple_framework_import.bzl
@@ -281,6 +281,7 @@ def _apple_dynamic_framework_import_impl(ctx):
         kind = "dynamic",
         label = label,
         libraries = [] if ctx.attr.bundle_only else framework.binary_imports,
+        swiftinterface_imports = framework.swift_interface_imports,
         swiftmodule_imports = framework.swift_module_imports,
     )
     providers.append(cc_info)
@@ -434,6 +435,7 @@ def _apple_static_framework_import_impl(ctx):
             label = label,
             libraries = framework.binary_imports,
             linkopts = linkopts,
+            swiftinterface_imports = framework.swift_interface_imports,
             swiftmodule_imports = framework.swift_module_imports,
         ),
     )

apple/internal/apple_xcframework_import.bzl
@@ -522,6 +522,7 @@ def _apple_dynamic_xcframework_import_impl(ctx):
         kind = "dynamic",
         label = label,
         libraries = [] if ctx.attr.bundle_only else [xcframework_library.binary],
+        swiftinterface_imports = [xcframework_library.swift_module_interface] if xcframework_library.swift_module_interface else [],
         swiftmodule_imports = xcframework_library.swiftmodule,
     )
     providers.append(cc_info)
@@ -667,6 +668,7 @@ def _apple_static_xcframework_import_impl(ctx):
         libraries = [xcframework_library.binary],
         framework_includes = xcframework_library.framework_includes,
         linkopts = sdk_linkopts + linkopts,
+        swiftinterface_imports = [],
         swiftmodule_imports = [],
         includes = xcframework_library.includes + ctx.attr.includes,
     )

apple/internal/framework_import_support.bzl
@@ -43,6 +43,7 @@ def _cc_info_with_dependencies(
         libraries,
         linkopts = [],
         includes = [],
+        swiftinterface_imports = [],
         swiftmodule_imports = [],
         is_framework = True):
     """Returns a new CcInfo which includes transitive Cc dependencies.
@@ -63,6 +64,8 @@ def _cc_info_with_dependencies(
         label: Label of the target being built.
         libraries: The list of framework libraries.
         linkopts: List of linker flags strings to propagate as linker input.
+        swiftinterface_imports: List of imported Swift interface files to include
+            during build phase, but aren't processed in any way.
         swiftmodule_imports: List of imported Swift module files to include during build phase,
             but aren't processed in any way.
         is_framework: Whether the target is a framework vs library.
@@ -83,6 +86,7 @@ def _cc_info_with_dependencies(
     public_hdrs = []
     public_hdrs.extend(header_imports)
     public_hdrs.extend(swiftmodule_imports)
+    public_hdrs.extend(swiftinterface_imports)
     (compilation_context, _compilation_outputs) = cc_common.compile(
         name = label.name,
         actions = actions,
@@ -178,17 +182,9 @@ def _classify_file_imports(config_vars, import_files):
             module_map_imports.append(file)
             continue
         if file_extension == "swiftmodule":
-            # Add Swift's module files to header_imports so
-            # that they are correctly included in the build
-            # by Bazel but they aren't processed in any way
-            header_imports.append(file)
             swift_module_imports.append(file)
             continue
         if file_extension == "swiftinterface":
-            # Add Swift's interface files to header_imports so
-            # that they are correctly included in the build
-            # by Bazel but they aren't processed in any way
-            header_imports.append(file)
             swift_interface_imports.append(file)
             continue
         if file_extension in ["swiftdoc", "swiftsourceinfo"]: