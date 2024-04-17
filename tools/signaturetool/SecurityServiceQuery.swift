ec96dda9d93e8867125f0fe64c588a1ed5eae424
Nicholas Levin <nglevin@google.com>
2024-04-17 09:38:43 -0700

    Handle retrieving information needed to report signed artifacts in Signatures XML via querying Security framework.
    PiperOrigin-RevId: 625712802

--- tools/signaturetool/SecurityServiceQuery.swift
+++ tools/signaturetool/SecurityServiceQuery.swift
@@ -37,23 +37,65 @@ public struct SecurityServiceQuery {
   /// Structure to represent the signature information itself and its format.
   public struct SignatureInfo: Codable, Equatable {
 
+    /// The signing identifier sealed into the signature; see Apple's kSecCodeInfoIdentifier docs.
+    /// Despite the name of the key, the value has no explicit connection to the bundle identifier.
+    public let bundleIdentifier: String?
+
+    /// An array containing the binary identifier for every digest algorithm supported.
+    public let cdhashes: [Data]?
+
+    /// An array of certificates representing the certificate chain of the signing certificate.
+    public let certificates: [Data]?
+
     /// Mandatory key that is always set to "false" in Xcode 15.3.
     public let isSecureTimestamp: Bool = false
 
     /// A dictionary of additional, optional values supplied by the build graph.
     public let metadata: [String: String]
 
+    /// A string representing the team identifier of the artifact and signing certificate.
+    public let signatureIdentifier: String?
+
+    /// A string indicating if the signature was adhoc ("SelfSigned") or came from a signing
+    /// identity furnished by Apple ("AppleDeveloperProgram").
+    public let signatureType: String?
+
     /// Indicates wheither the artifact was code signed.
     public let signed: Bool
 
-    public init(metadataInfo: [CollectSignaturesOptions.MetadataInfo], signed: Bool) {
+    /// The source of the code signature used for the code object.
+    public let source: String?
+
+    init(
+      cdhashes: [Data]? = nil, certificates: [SecCertificate]? = nil,
+      flags: SecCodeSignatureFlags.RawValue? = nil, identifier: String? = nil,
+      metadataInfo: [CollectSignaturesOptions.MetadataInfo], source: String? = nil,
+      teamIdentifier: String? = nil
+    ) {
+      self.bundleIdentifier = identifier
+      self.cdhashes = cdhashes
+      self.certificates = certificates?.map { SecCertificateCopyData($0) as Data }
       self.metadata = Dictionary(
         uniqueKeysWithValues: metadataInfo.map { ($0.key, $0.value) })
-      self.signed = signed
+      self.signatureIdentifier = teamIdentifier
+      if let rawFlags = flags {
+        self.signatureType =
+          SecCodeSignatureFlags(rawValue: rawFlags).contains(.adhoc)
+          ? "SelfSigned" : "AppleDeveloperProgram"
+      } else {
+        self.signatureType = nil
+      }
+      // Per Apple documentation for SecCodeCopySigningInformation, an absent kSecCodeInfoIdentifier
+      // key indicates that the object is unsigned.
+      self.signed = identifier != nil
+      self.source = source
     }
 
     private enum CodingKeys: String, CodingKey {
-      case isSecureTimestamp, metadata, signed
+      // common_typos_disable - The misspelled key "bundleIndentifier" is what it is for Xcode 15.3.
+      case bundleIdentifier = "bundleIndentifier", cdhashes, certificates, isSecureTimestamp,
+        metadata, signatureIdentifier, signatureType, signed, source
+      // common_typos_enable
     }
   }
 
@@ -97,22 +139,17 @@ public struct SecurityServiceQuery {
     throws -> SignatureInfo
   {
     let secStaticCode = try self.secStaticCode()
-
-    // First, get the signing information dictionary, ignoring validation.
     let infoDictionary = try self.signingInformation(secStaticCode: secStaticCode)
+    let identifier = infoDictionary[kSecCodeInfoIdentifier as String] as? String
 
-    // Per Apple documentation for SecCodeCopySigningInformation, an absent kSecCodeInfoIdentifier
-    // key indicates that the object is unsigned. Return a simple dictionary indicating as much,
-    // matching the output of xcodebuild as of Xcode 15.3.
-    if infoDictionary[kSecCodeInfoIdentifier as String] == nil {
-      return SignatureInfo(metadataInfo: metadataInfo, signed: false)
-    }
-
-    // TODO(b/326280185): If the object is signed, convert the signing information dictionary into
-    // an encodable XML plist-safe form, with Data objects instead of custom Core Foundation or
-    // Security structures and types.
-
-    return SignatureInfo(metadataInfo: metadataInfo, signed: true)
+    return SignatureInfo(
+      cdhashes: infoDictionary[kSecCodeInfoCdHashes as String] as? [Data],
+      certificates: infoDictionary[kSecCodeInfoCertificates as String] as? [SecCertificate],
+      flags: infoDictionary[kSecCodeInfoFlags as String] as? SecCodeSignatureFlags.RawValue,
+      identifier: identifier,
+      metadataInfo: metadataInfo,
+      source: infoDictionary[kSecCodeInfoSource as String] as? String,
+      teamIdentifier: infoDictionary[kSecCodeInfoTeamIdentifier as String] as? String)
   }
 
 }
