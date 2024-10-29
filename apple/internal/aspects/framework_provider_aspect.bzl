bazelbuild/rules_apple

commit 942c213f7e32797ca4f010de4e9d7b99e45c84e8

Add support for rules_swift `mixed_language_library` (#2575)

apple/internal/aspects/framework_provider_aspect.bzl
@@ -36,6 +36,9 @@ _FRAMEWORK_PROVIDERS_ASPECT_ATTRS = [
     "implementation_deps",
     "private_deps",
     "runtime_deps",
+    # rules_swift `mixed_language_target`
+    "clang_target",
+    "swift_target",
 ]
 
 def _framework_provider_aspect_impl(target, ctx):

apple/internal/aspects/resource_aspect.bzl
@@ -430,6 +430,9 @@ apple_resource_aspect = aspect(
         "private_deps",
         "structured_resources",
         "resources",
+        # rules_swift `mixed_language_target`
+        "clang_target",
+        "swift_target",
     ],
     attrs = dicts.add(
         apple_support.action_required_attrs(),

commit fd06db099fd2a92a3a04399bcc41d1291b33da46

Fix `mixed_language_library`
Regressed with 942c213f7e32797ca4f010de4e9d7b99e45c84e8.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

apple/internal/aspects/framework_provider_aspect.bzl
@@ -36,9 +36,6 @@ _FRAMEWORK_PROVIDERS_ASPECT_ATTRS = [
     "implementation_deps",
     "private_deps",
     "runtime_deps",
-    # rules_swift `mixed_language_target`
-    "clang_target",
-    "swift_target",
 ]
 
 def _framework_provider_aspect_impl(target, ctx):