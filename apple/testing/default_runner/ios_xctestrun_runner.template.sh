baf091bcaa96ddd9b26dc9c469d138b133e6a6d1
Aaron Sky <asky@dropbox.com>
2025-02-10 21:31:14 -0500

    insert pre- and post- actions in ios_xctestrun_runner

--- apple/testing/default_runner/ios_xctestrun_runner.template.sh
+++ apple/testing/default_runner/ios_xctestrun_runner.template.sh
@@ -134,11 +134,12 @@ fi
 
 # Add the test environment variables into the xctestrun file to propagate them
 # to the test runner
+default_test_env="TEST_SRCDIR=$TEST_SRCDIR,TEST_UNDECLARED_OUTPUTS_DIR=$TEST_UNDECLARED_OUTPUTS_DIR,XML_OUTPUT_FILE=$XML_OUTPUT_FILE"
 test_env="%(test_env)s"
 if [[ -n "$test_env" ]]; then
-  test_env="$test_env,TEST_SRCDIR=$TEST_SRCDIR,TEST_UNDECLARED_OUTPUTS_DIR=$TEST_UNDECLARED_OUTPUTS_DIR,XML_OUTPUT_FILE=$XML_OUTPUT_FILE"
+  test_env="$test_env,$default_test_env"
 else
-  test_env="TEST_SRCDIR=$TEST_SRCDIR,TEST_UNDECLARED_OUTPUTS_DIR=$TEST_UNDECLARED_OUTPUTS_DIR,XML_OUTPUT_FILE=$XML_OUTPUT_FILE"
+  test_env="$default_test_env"
 fi
 
 passthrough_env=()
@@ -427,6 +428,8 @@ if (( ${#custom_xcodebuild_args[@]} )); then
   should_use_xcodebuild=true
 fi
 
+%(pre_action_binary)s
+
 if [[ "$should_use_xcodebuild" == true ]]; then
   if [[ -z "$test_host_path" && "$intel_simulator_hack" == true ]]; then
     echo "error: running x86_64 tests on arm64 macs using 'xcodebuild' requires a test host" >&2
@@ -523,6 +526,8 @@ else
     2>&1 | tee -i "$testlog" | (grep -v "One of the two will be used" || true) \
     || test_exit_code=$?
 fi
+
+%(post_action_binary)s
 
 if [[ "$reuse_simulator" == false ]]; then
   # Delete will shutdown down the simulator if it's still currently running.
