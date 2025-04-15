19ca6d8674e7fcce77a6c6caf0895558a8dd7bb9
Aaron Sky <aaronsky@skyaaron.com>
2025-04-15 17:03:30 -0400

    repro of #2687: add a new framework to `//examples/multi_platform/Buttons` that exhibits the failure case

--- examples/multi_platform/Buttons/ButtonsExtension/ActionRequestHandler.swift
+++ examples/multi_platform/Buttons/ButtonsExtension/ActionRequestHandler.swift
@@ -12,8 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import ButtonsShared
+import ButtonsShared2
 import UIKit
 
 class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
-  func beginRequest(with context: NSExtensionContext) { }
+  func beginRequest(with context: NSExtensionContext) {
+    saySomethingShared()
+    saySomethingElseShared()
+  }
 }
