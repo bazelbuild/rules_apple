bazelbuild/rules_apple

commit d24030274511aebedd6a90d1fc344ad78380ef8b

Align actool with upstream (#2804)

apple/internal/bundling_support.bzl
@@ -15,6 +15,10 @@
 """Low-level bundling name helpers."""
 
 load(
+    "@bazel_skylib//lib:new_sets.bzl",
+    "sets",
+)
+load(
     "//apple:providers.bzl",
     "AppleBaseBundleIdInfo",
     "AppleSharedCapabilityInfo",
@@ -248,24 +252,37 @@ See https://github.com/bazelbuild/rules_apple/blob/master/doc/shared_capabilitie
         suffix_default = suffix_default,
     )
 
-def _ensure_single_xcassets_type(
+def _ensure_asset_catalog_files_not_in_xcassets(
         *,
-        additional_path_fragments = [],
-        attr,
         extension,
         files,
         message = None):
-    """Helper for when an xcassets catalog should have a single sub type.
+    """Validates that a subset of asset catalog files are not within an xcassets directory.
 
     Args:
-      additional_path_fragments: Additional path fragments to check for in the path (in order), used
-          to handle actool inputs that don't use the `xcassets` directory, such as Xcode 26 `icon`
-          bundles generated by the Icon Composer tool.
-      attr: The attribute to associate with the build failure if the list of
-          files has an element that is not in a directory with the given
-          extension.
-      extension: The extension that should be used for the different asset
-          type witin the catalog.
+      extension: The extension that should be used for the this particular asset that should never
+          be found within the xcassets directory.
+      files: An iterable of files to use.
+      message: A custom error message to use, the list of found files that were found in xcassets
+          directories will be printed afterwards.
+    """
+    _ensure_path_format(
+        files = files,
+        allowed_path_fragments = [],
+        denied_path_fragments = ["xcassets", extension],
+        message = message,
+    )
+
+def _ensure_single_xcassets_type(
+        *,
+        extension,
+        files,
+        message = None):
+    """Validates that asset catalog files are nested within an xcassets directory.
+
+    Args:
+      extension: The extension that should be used for the this particular asset within the xcassets
+          directory.
       files: An iterable of files to use.
       message: A custom error message to use, the list of found files that
           didn't match will be printed afterwards.
@@ -274,9 +291,9 @@ def _ensure_single_xcassets_type(
         message = ("Expected the xcassets directory to only contain files " +
                    "are in sub-directories with the extension %s") % extension
     _ensure_path_format(
-        attr = attr,
         files = files,
-        path_fragments_list = [["xcassets", extension], additional_path_fragments],
+        allowed_path_fragments = ["xcassets", extension],
+        denied_path_fragments = [],
         message = message,
     )
 
@@ -307,63 +324,55 @@ def _path_is_under_fragments(path, path_fragments):
 
     return True
 
-def _ensure_path_format(*, attr, files, path_fragments_list, message = None):
+def _ensure_path_format(
+        *,
+        files,
+        allowed_path_fragments,
+        denied_path_fragments,
+        message = None):
     """Ensure the files match the required path fragments.
 
-    TODO(b/77804841): The places calling this should go away and these types of
-    checks should be done during the resource processing. Right now these checks
-    are being wedged in at the attribute collection steps, and they then get
-    combined into a single list of resources; the bundling then resplits them
-    up in groups to process they by type. So the more validation/splitting done
-    here the slower things get (as double work is done). The bug is to revisit
-    all of this and instead pass through individual things in a structured way
-    so they don't have to be resplit. That would allow the validation to be
-    done while processing (in a single pass) instead.
-
     Args:
-      attr: The attribute to associate with the build failure if the list of
-          files has an element that is not in a directory with the given
-          extension.
       files: An iterable of files to use.
-      path_fragments_list: A list of lists, each inner lists is a sequence of
-          extensions that must be on the paths passed in (to ensure proper
-          nesting).
+      allowed_path_fragments: A list representing a sequence of extensions where each file path
+          passed in MUST MATCH the sequence to ensure proper nesting. If this is provided,
+          denied_path_fragments must be empty.
+      denied_path_fragments: A list representing a sequence of extensions where each file path
+          passed in MUST NOT MATCH the sequence to ensure proper nesting. If this is provided,
+          allowed_path_fragments must be empty.
       message: A custom error message to use, the list of found files that
           didn't match will be printed afterwards.
     """
 
-    formatted_path_fragments_list = []
-    for x in path_fragments_list:
-        formatted_path_fragments_list.append([".%s/" % y for y in x])
+    if allowed_path_fragments and denied_path_fragments:
+        fail("""
+Internal Error: Both allowed_path_fragments and denied_path_fragments were provided, but only one \
+of them should be provided.
 
-    # Just check that the paths include the expected nesting. More complete
-    # checks would likely be the number of outer directories with that suffix,
-    # the number of inner ones, extra directories segments where not expected,
-    # etc.
-    bad_paths = {}
+Please file an issue on the Apple BUILD Rules.
+""")
+
+    formatted_path_fragments = []
+    for x in allowed_path_fragments + denied_path_fragments:
+        formatted_path_fragments.append(".%s/" % x)
+    allow_path_under_fragments = bool(allowed_path_fragments)
+
+    bad_paths = sets.make()
     for f in files:
         path = f.path
+        if _path_is_under_fragments(path, formatted_path_fragments) != allow_path_under_fragments:
+            sets.insert(bad_paths, path)
 
-        was_good = False
-        for path_fragments in formatted_path_fragments_list:
-            if _path_is_under_fragments(path, path_fragments):
-                was_good = True
-                break  # No need to check other fragments
-
-        if not was_good:
-            bad_paths[path] = None
-
-    if len(bad_paths):
+    if sets.length(bad_paths):
         if not message:
-            as_paths = [
-                ("*" + "*".join(x) + "...")
-                for x in formatted_path_fragments_list
-            ]
-            message = "Expected only files inside directories named '*.%s'" % (
-                ", ".join(as_paths)
+            message_prefix = (
+                "Expected only " if allow_path_under_fragments else "Did not expect any "
             )
-        formatted_paths = "[\n  %s\n]" % ",\n  ".join(bad_paths.keys())
-        fail("%s, but found the following: %s" % (message, formatted_paths), attr)
+            as_path = "*" + "*".join(formatted_path_fragments) + "..."
+            message = message_prefix + "files inside directories named '*.%s'" % (as_path)
+
+        formatted_paths = "[\n  %s\n]" % ",\n  ".join(sets.to_list(bad_paths))
+        fail("%s, but found the following: %s" % (message, formatted_paths))
 
 def _validate_bundle_id(bundle_id):
     """Ensure the value is a valid bundle it or fail the build.
@@ -393,7 +402,7 @@ def _validate_bundle_id(bundle_id):
 bundling_support = struct(
     bundle_full_name = _bundle_full_name,
     bundle_full_id = _bundle_full_id,
-    ensure_path_format = _ensure_path_format,
+    ensure_asset_catalog_files_not_in_xcassets = _ensure_asset_catalog_files_not_in_xcassets,
     ensure_single_xcassets_type = _ensure_single_xcassets_type,
     validate_bundle_id = _validate_bundle_id,
 )

apple/internal/partials/app_assets_validation.bzl
@@ -22,12 +22,26 @@ load(
     "//apple/internal:apple_product_type.bzl",
     "apple_product_type",
 )
-load(
-    "//apple/internal:bundling_support.bzl",
-    "bundling_support",
-)
 
-_supports_visionos = hasattr(apple_common.platform_type, "visionos")
+# Standard icon extensions as of Xcode 26 for most Apple platforms (iOS, macOS, watchOS).
+_STANDARD_ICONS = [".appiconset/", ".icon/"]
+
+# Valid icon extensions for specific product types that have exceptional requirements, independent
+# of platform.
+_VALID_ICON_EXTENSIONS_FOR_PRODUCT_TYPE = {
+    apple_product_type.messages_extension: [".stickersiconset/"],
+    apple_product_type.messages_sticker_pack_extension: [".stickersiconset/", ".stickerpack/", ".sticker/", ".stickersequence/"],
+}
+
+# Comprehensive list of all valid icon extensions for each platform. These cover apps, extensions,
+# app clips, and bundle types that can use icons.
+_VALID_ICON_EXTENSIONS_FOR_PLATFORM = {
+    "ios": _STANDARD_ICONS,
+    "macos": _STANDARD_ICONS,
+    "tvos": [".brandassets/"],
+    "watchos": _STANDARD_ICONS,
+    "visionos": [".solidimagestack/"],
+}
 
 def _app_assets_validation_partial_impl(
         *,
@@ -37,74 +51,49 @@ def _app_assets_validation_partial_impl(
         product_type):
     """Implementation for the app assets processing partial."""
 
+    # actool.bzl has the most comprehensive validations since it evaluates the final set of files
+    # before they are sent to actool. We only check here that the user is sending files that look
+    # like they could be app icons via an attribute named `app_icons`, and likewise for launch
+    # images.
+
     if app_icons:
-        if product_type == apple_product_type.messages_extension:
-            message = ("Message extensions must use Messages Extensions Icon Sets " +
-                       "(named .stickersiconset), not traditional App Icon Sets")
-            bundling_support.ensure_single_xcassets_type(
-                attr = "app_icons",
-                extension = "stickersiconset",
-                files = app_icons,
-                message = message,
-            )
-        elif product_type == apple_product_type.messages_sticker_pack_extension:
-            path_fragments = [
-                # Replacement for appiconset.
-                ["xcstickers", "stickersiconset"],
-                # The stickers.
-                ["xcstickers", "stickerpack", "sticker"],
-                ["xcstickers", "stickerpack", "stickersequence"],
-            ]
-            message = (
-                "Message StickerPack extensions use an asset catalog named " +
-                "*.xcstickers. Their main icons use *.stickersiconset; and then " +
-                "under the Sticker Pack (*.stickerpack) goes the Stickers " +
-                "(named *.sticker) and/or Sticker Sequences (named " +
-                "*.stickersequence)"
-            )
-            bundling_support.ensure_path_format(
-                attr = "app_icons",
-                files = app_icons,
-                path_fragments_list = path_fragments,
-                message = message,
-            )
-        elif platform_prerequisites.platform_type == apple_common.platform_type.tvos:
-            bundling_support.ensure_single_xcassets_type(
-                attr = "app_icons",
-                extension = "brandassets",
-                files = app_icons,
-            )
-        elif (_supports_visionos and
-              platform_prerequisites.platform_type == apple_common.platform_type.visionos):
-            message = ("visionOS apps must use visionOS app icon layers grouped in " +
-                       ".solidimagestack bundles, not traditional App Icon Sets")
-            bundling_support.ensure_single_xcassets_type(
-                attr = "app_icons",
-                extension = "solidimagestack",
-                files = app_icons,
-                message = message,
-            )
-        else:
-            # For Xcode 26, appiconset-s can live alongside iOS/macOS/watchOS .icon bundle contents
-            # to ensure rendering is correct for pre-26 Apple OSes, so we need to make that
-            # exception here.
-            additional_path_fragments = []
-            xcode_version = platform_prerequisites.xcode_version_config.xcode_version()
-            if xcode_version >= apple_common.dotted_version("26.0"):
-                additional_path_fragments.append("icon")
-            bundling_support.ensure_single_xcassets_type(
-                additional_path_fragments = additional_path_fragments,
-                attr = "app_icons",
-                extension = "appiconset",
-                files = app_icons,
-            )
+        valid_icon_extensions = (
+            _VALID_ICON_EXTENSIONS_FOR_PRODUCT_TYPE.get(product_type, None) or
+            _VALID_ICON_EXTENSIONS_FOR_PLATFORM[str(platform_prerequisites.platform_type)]
+        )
+        for resource in app_icons:
+            resource_short_path = resource.short_path
+            possible_valid_icon = False
+            for valid_icon_extension in valid_icon_extensions:
+                if valid_icon_extension in resource_short_path:
+                    possible_valid_icon = True
+                    break
+            if (not possible_valid_icon and
+                not resource_short_path.endswith(".xcassets/Contents.json") and
+                not resource_short_path.endswith(".xcstickers/Contents.json")):
+                fail("""
+Found in app_icons a file that cannot be used as an app icon:
+{resource_short_path}
+
+Valid icon bundles for this target have the following extensions: {valid_icon_extensions}
+""".format(
+                    resource_short_path = resource_short_path,
+                    valid_icon_extensions = valid_icon_extensions,
+                ))
 
     if launch_images:
-        bundling_support.ensure_single_xcassets_type(
-            attr = "launch_images",
-            extension = "launchimage",
-            files = launch_images,
-        )
+        for resource in launch_images:
+            resource_short_path = resource.short_path
+            if (not ".launchimage/" in resource_short_path and
+                not resource_short_path.endswith(".xcassets/Contents.json")):
+                fail("""
+Found in launch_images a file that cannot be used as a launch image:
+{resource_short_path}
+
+All launch images must be in a directory named '*.launchimage' within an '*.xcassets' directory.
+""".format(
+                    resource_short_path = resource_short_path,
+                ))
 
     return struct()
 

apple/internal/partials/BUILD
@@ -15,7 +15,6 @@ bzl_library(
     ],
     deps = [
         "//apple/internal:apple_product_type",
-        "//apple/internal:bundling_support",
         "@bazel_skylib//lib:partial",
     ],
 )

apple/internal/resource_actions/BUILD
@@ -16,9 +16,9 @@ bzl_library(
     deps = [
         "//apple:utils",
         "//apple/internal:apple_product_type",
+        "//apple/internal:bundling_support",
         "//apple/internal/utils:xctoolrunner",
         "@bazel_skylib//lib:paths",
-        "@bazel_skylib//lib:sets",
         "@build_bazel_apple_support//lib:apple_support",
     ],
 )

test/ios_imessage_test.sh
@@ -211,9 +211,10 @@ EOF
   ! do_build ios //app:app || fail "Should fail build"
 
   # Check for the start of the log message
-  expect_log "Message StickerPack extensions use an asset catalog named "
-  # The 9 icons and the Contents.json should all be listed, so 10 hits.
-  expect_log_n "testdata/resources/app_icons_ios.xcassets/app_icon.appiconset/" 10
+  expect_log "Found in app_icons a file that cannot be used as an app icon"
+  expect_log "Valid icon bundles for this target have the following extensions"
+  # Verify the invalid appiconset path is mentioned
+  expect_log "app_icons_ios.xcassets/app_icon.appiconset"
 }
 
 run_suite "imessage bundling resource tests"

test/starlark_tests/common.bzl
@@ -34,6 +34,8 @@ _min_os_ios = struct(
     appclip_support = "14.0",
     arm_sim_support = "14.0",
     baseline = "12.0",
+    concurrency_support = "13.0",
+    icon_bundle_required = "26.0",
     oldest_supported = "11.0",
     nplus1 = "13.0",
     stable_swift_abi = "12.2",
@@ -44,6 +46,9 @@ _min_os_macos = struct(
     app_intents_support = "13.0",
     arm64_support = "11.0",
     baseline = "10.13",
+    icon_bundle_required = "26.0",
+    requires_concurrency_stdlib = "10.14",
+    arm64_support_plus1 = "12.0",
 )
 
 # The current baseline for tvOS is version 12.0, based on when the arm64e architecture was
@@ -67,6 +72,7 @@ _min_os_watchos = struct(
     arm64_support = "9.0",
     arm_sim_support = "7.0",
     baseline = "4.0",
+    icon_bundle_required = "26.0",
     requires_single_target_app = "9.0",
     single_target_app = "7.0",
     stable_swift_abi = "6.0",

test/starlark_tests/ios_application_resources_test.bzl
@@ -203,6 +203,37 @@ def ios_application_resources_test_suite(name):
         build_type = "device",
         target_under_test = "//test/starlark_tests/targets_under_test/ios:app_with_icon_bundle",
         contains = [
+            "$BUNDLE_ROOT/Assets.car",
+        ],
+        plist_test_file = "$CONTENT_ROOT/Info.plist",
+        plist_test_values = {
+            "CFBundleIcons:CFBundlePrimaryIcon:CFBundleIconName": "app_icon",
+        },
+        # Skip CI until CI is on Xcode 26
+        tags = [name] + common.fixture_tags + common.skip_ci_tags,
+    )
+
+    # Test a failure when the new icon composer bundles for Xcode 26 are mixed with a set of asset \
+    # catalog icons.
+    analysis_failure_message_test(
+        name = "{}_icon_composer_and_asset_catalog_app_icons_failure_test".format(name),
+        target_under_test = "//test/starlark_tests/targets_under_test/ios:app_with_icon_bundle_and_xcassets_app_icons",
+        expected_error = """
+            Found .appiconset files among the assigned app_icons, which are ignored when Icon \
+            Composer .icon bundles are present.
+            """,
+        # Skip CI until CI is on Xcode 26
+        tags = [name] + common.skip_ci_tags,
+    )
+
+    # Tests that icon bundles alone will generate legacy assets when the minimum_os_version is lower
+    # than 26.0.
+    archive_contents_test(
+        name = "{}_icon_bundles_for_minimum_os_version_below_26_test".format(name),
+        build_type = "device",
+        target_under_test = "//test/starlark_tests/targets_under_test/ios:app_with_icon_bundle_only_for_low_minimum_os_version",
+        contains = [
+            "$BUNDLE_ROOT/Assets.car",
             "$BUNDLE_ROOT/app_icon76x76@2x~ipad.png",
             "$BUNDLE_ROOT/app_icon60x60@2x.png",
         ],
@@ -215,22 +246,42 @@ def ios_application_resources_test_suite(name):
         tags = [name] + common.fixture_tags + common.skip_ci_tags,
     )
 
-    # Tests the new icon composer bundles for Xcode 26, along with a set of asset catalog icons.
-    archive_contents_test(
-        name = "{}_icon_composer_and_asset_catalog_app_icons_plist_test".format(name),
-        build_type = "device",
-        target_under_test = "//test/starlark_tests/targets_under_test/ios:app_with_icon_bundle_and_xcassets_app_icons",
-        contains = [
-            "$BUNDLE_ROOT/app_icon76x76@2x~ipad.png",
-            "$BUNDLE_ROOT/app_icon60x60@2x.png",
-        ],
-        plist_test_file = "$CONTENT_ROOT/Info.plist",
-        plist_test_values = {
-            "CFBundleIcons:CFBundlePrimaryIcon:CFBundleIconFiles:0": "app_icon60x60",
-            "CFBundleIcons:CFBundlePrimaryIcon:CFBundleIconName": "app_icon",
-        },
+    # Tests that icon composer icons must be provided when the minimum_os_version is 26.0 or higher.
+    analysis_failure_message_test(
+        name = "{}_legacy_app_icons_for_minimum_os_version_26_test".format(name),
+        target_under_test = "//test/starlark_tests/targets_under_test/ios:app_with_xcassets_for_ios_26",
+        expected_error = """
+Legacy .appiconset files should not be used on iOS/macOS/watchOS 26+.
+
+These platforms prefer Icon Composer .icon bundles. .appiconset files are only needed for rendering icons in iOS/macOS/watchOS prior to 26.
+
+Found the following legacy .appiconset files: """,
         # Skip CI until CI is on Xcode 26
-        tags = [name] + common.fixture_tags + common.skip_ci_tags,
+        tags = [name] + common.skip_ci_tags,
+    )
+
+    # Test a failure when new icon composer bundles for Xcode 26 are mixed with a set of asset
+    # catalog icons in an iOS app that provides alternate app icons.
+    analysis_failure_message_test(
+        name = "{}_icon_composer_and_asset_catalog_app_icons_with_alternate_app_icons_failure_test".format(name),
+        target_under_test = "//test/starlark_tests/targets_under_test/ios:app_with_alternate_app_icons_with_full_icon_bundle_coverage",
+        expected_error = """
+            Found .appiconset files among the assigned app_icons, which are ignored when Icon \
+            Composer .icon bundles are present.
+            """,
+        # Skip CI until CI is on Xcode 26
+        tags = [name] + common.skip_ci_tags,
+    )
+
+    # Tests that icon composer icons will be flagged when building against Xcode 16 instead of 26.
+    analysis_failure_message_test(
+        name = "{}_test_xcode_16_with_icon_composer_icons_test".format(name),
+        target_under_test = "//test/starlark_tests/targets_under_test/ios:app_with_icon_bundle_only_for_low_minimum_os_version",
+        expected_error = """
+Found Icon Composer .icon bundles among the assigned app_icons. These are only supported on Xcode 26 or later.""",
+        tags = [
+            name,
+        ] + common.skip_ci_tags,
     )
 
     # Tests that the launch storyboard is bundled with the application and that
@@ -279,14 +330,8 @@ def ios_application_resources_test_suite(name):
         expected_error = """
 Found multiple app icons among the asset catalogs with no primary_app_icon assigned.
 
-If you intend to assign multiple app icons to this target, please declare which of these is intended
-to be the primary app icon with the primary_app_icon attribute on the rule itself.
-
-app_icons was assigned the following: [
-  test/testdata/resources/app_icons_with_alts_ios.xcassets/app_icon-bazel.appiconset,
-  test/testdata/resources/app_icons_with_alts_ios.xcassets/app_icon.appiconset
-]
-""",
+If you intend to assign multiple app icons to this target, please declare which of these is \
+intended to be the primary app icon with the primary_app_icon attribute on the rule itself.""",
         tags = [name],
     )
 

commit 20d696cce417f14d54bf256070df1197385b05ba

Fix `.xcstickers` app icon regression
Regressed in d24030274511aebedd6a90d1fc344ad78380ef8b.

Signed-off-by: Brentley Jones <github@brentleyjones.com>

apple/internal/bundling_support.bzl
@@ -256,7 +256,8 @@ def _ensure_asset_catalog_files_not_in_xcassets(
         *,
         extension,
         files,
-        message = None):
+        message = None,
+        xcassets_extension = "xcassets"):
     """Validates that a subset of asset catalog files are not within an xcassets directory.
 
     Args:
@@ -265,11 +266,13 @@ def _ensure_asset_catalog_files_not_in_xcassets(
       files: An iterable of files to use.
       message: A custom error message to use, the list of found files that were found in xcassets
           directories will be printed afterwards.
+      xcassets_extension: The extension of the xcassets directory. Normally `xcassets`, but other
+          variations exist (e.g. `xcstickers`).
     """
     _ensure_path_format(
         files = files,
         allowed_path_fragments = [],
-        denied_path_fragments = ["xcassets", extension],
+        denied_path_fragments = [xcassets_extension, extension],
         message = message,
     )
 
@@ -277,7 +280,8 @@ def _ensure_single_xcassets_type(
         *,
         extension,
         files,
-        message = None):
+        message = None,
+        xcassets_extension = "xcassets"):
     """Validates that asset catalog files are nested within an xcassets directory.
 
     Args:
@@ -286,13 +290,15 @@ def _ensure_single_xcassets_type(
       files: An iterable of files to use.
       message: A custom error message to use, the list of found files that
           didn't match will be printed afterwards.
+      xcassets_extension: The extension of the xcassets directory. Normally `xcassets`, but other
+          variations exist (e.g. `xcstickers`).
     """
     if not message:
-        message = ("Expected the xcassets directory to only contain files " +
-                   "are in sub-directories with the extension %s") % extension
+        message = ("Expected the %s directory to only contain files " +
+                   "are in sub-directories with the extension %s") % (xcassets_extension, extension)
     _ensure_path_format(
         files = files,
-        allowed_path_fragments = ["xcassets", extension],
+        allowed_path_fragments = [xcassets_extension, extension],
         denied_path_fragments = [],
         message = message,
     )

apple/internal/partials/app_assets_validation.bzl
@@ -30,7 +30,15 @@ _STANDARD_ICONS = [".appiconset/", ".icon/"]
 # of platform.
 _VALID_ICON_EXTENSIONS_FOR_PRODUCT_TYPE = {
     apple_product_type.messages_extension: [".stickersiconset/"],
-    apple_product_type.messages_sticker_pack_extension: [".stickersiconset/", ".stickerpack/", ".sticker/", ".stickersequence/"],
+    apple_product_type.messages_sticker_pack_extension: [
+        # Replacement for appiconset.
+        ".stickersiconset/",
+        # The stickers. Ideally we wouldn't list these here, but `app_assets_validation_partial`
+        # is called with `sticker_assets` instead of just the app icons.
+        ".stickerpack/",
+        ".sticker/",
+        ".stickersequence/",
+    ],
 }
 
 # Comprehensive list of all valid icon extensions for each platform. These cover apps, extensions,

apple/internal/resource_actions/actool.bzl
@@ -79,6 +79,33 @@ Found the following: {icon_bundle_files}
 
 """.format(icon_bundle_files = icon_bundle_files))
 
+def _validate_sticker_pack_icon_sets(*, icon_bundle_files, stickers_icon_files, xcasset_appicon_files):
+    """Validates that the asset files contain only sticker icon sets."""
+    message = ("Sticker pack extensions must use Sticker Icon Sets " +
+               "(named .stickersiconset), not traditional App Icon Sets")
+    bundling_support.ensure_single_xcassets_type(
+        extension = "stickersiconset",
+        files = stickers_icon_files,
+        message = message,
+        xcassets_extension = "xcstickers",
+    )
+
+    # Check that there are no .appiconset files, which are not allowed for messages extensions.
+    bundling_support.ensure_asset_catalog_files_not_in_xcassets(
+        extension = "appiconset",
+        files = xcasset_appicon_files,
+        message = message,
+        xcassets_extension = "xcstickers",
+    )
+
+    if icon_bundle_files:
+        fail("""
+Icon Composer .icon bundles are not supported for Messages Extensions.
+
+Found the following: {icon_bundle_files}
+
+""".format(icon_bundle_files = icon_bundle_files))
+
 def _validate_tvos_icon_sets(*, brandassets_icon_files, icon_bundle_files, xcasset_appicon_files):
     """Validates that the asset files contain only tvOS brand assets."""
     message = ("tvOS apps must use tvOS brand assets (named .brandassets), " +
@@ -198,6 +225,15 @@ def _icon_info_from_asset_files(
             stickers_icon_files = icon_files,
             xcasset_appicon_files = xcasset_appicon_files,
         )
+    elif product_type == apple_product_type.messages_sticker_pack_extension:
+        appicon_extension = "stickersiconset"
+        icon_files = [f for f in asset_files if ".stickersiconset/" in f.path]
+        _validate_sticker_pack_icon_sets(
+            icon_bundle_files = icon_bundle_files,
+            stickers_icon_files = icon_files,
+            xcasset_appicon_files = xcasset_appicon_files,
+        )
+
     elif platform_type == "tvos":
         appicon_extension = "brandassets"
         icon_files = [f for f in asset_files if ".brandassets/" in f.path]
@@ -298,7 +334,10 @@ def _args_for_app_icons(
         product_type):
     """Returns arguments for app icons."""
     args = []
-    if product_type == apple_product_type.messages_extension:
+    if product_type in [
+        apple_product_type.messages_extension,
+        apple_product_type.messages_sticker_pack_extension,
+    ]:
         args.extend([
             "--include-sticker-content",
             "--stickers-icon-role",